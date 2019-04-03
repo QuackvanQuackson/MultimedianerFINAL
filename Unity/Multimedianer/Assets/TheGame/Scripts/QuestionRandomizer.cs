b7a1dda8cbe9c42a1f281ba55fea3af54378a52f
QuackvanQuackson <richard.lindner13@gmail.com>
2019-04-03 12:37:02 +0200

    Question/Answers and Randomize changes

--- Unity/Multimedianer/Assets/TheGame/Scripts/QuestionRandomizer.cs
+++ Unity/Multimedianer/Assets/TheGame/Scripts/QuestionRandomizer.cs
@@ -4,26 +4,43 @@ using UnityEngine.UI;
 
 public class QuestionRandomizer : MonoBehaviour
 {
+    public Text textQuestion;
+    public Text textAnswer1;
+    public Text textAnswer2;
+    public Text textAnswer3;
+    public Text textAnswer4;
     private const string QUESTIONDISPLAY = "QuestionDisplay";
     public Question[] questions = new Question[10];
 
     public void ChangeText ()
     {
-        List<string> questions = new List<string> ();
+        /*List<string> questions = new List<string> ();
 
+        questions.Add("Was ist ein Event?");
+        questions.Add("Was sind die meistbenutzten Schleifen?");
+        questions.Add("Was versteht man unter Bug");
         questions.Add("Was bedeutet HTML?");
-        questions.Add("Was ist bei einem HTML Document der erste Tag?");
-        questions.Add("Womit werden die Zeilen in C# getrennt?");
-        questions.Add("Wofür steht css?");
-        questions.Add("Was ist kein command für Git?");
+        questions.Add("Was ist bei einem HTML Dokument der erste Tag?");
+        questions.Add("Mit welchem Variablentyp kann eine Gleitkommazahl gespeichert werden?");
+        questions.Add("Wofür steht CSS?");
+        questions.Add("Welcher Command ist für Git?");
+        questions.Add("Wofür steht SDK?");
+        questions.Add("Was ist ein Kommentar?");
+        //questions.Add("Was bedeutet HTML?");
+        //questions.Add("Was ist bei einem HTML Document der erste Tag?");
+        //questions.Add("Womit werden die Zeilen in C# getrennt?");
+        //questions.Add("Wofür steht css?");
+        //questions.Add("Was ist kein command für Git?");
         // 10 Fragen minimum
+        */
 
-        int questionNumber = Random.Range (0,5);
+        int questionNumber = Random.Range (0,9);
 
         var QuestionRandom = GameObject.Find (QUESTIONDISPLAY);
 
         var text = QuestionRandom.GetComponent<Text> ();
 
-        text.text = questions [questionNumber];
+        textQuestion.text = questions [questionNumber].question;
+
     }
 }
