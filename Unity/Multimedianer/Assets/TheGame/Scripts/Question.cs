63e65d48f282f6bfa549108e61e53164930d6510
QuackvanQuackson <richard.lindner13@gmail.com>
2019-04-03 11:25:58 +0200

    Question Script added

---
+++ Unity/Multimedianer/Assets/TheGame/Scripts/Question.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "QuestionData")]
+public class Question : ScriptableObject
+{
+    public string question = "";
+    //0 richtig, 1-3 falsch
+    public string[] answers = new string[4];
+}
